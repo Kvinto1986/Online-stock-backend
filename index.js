cd0fea47270b6c346a927d9e811b010a0bf3ddb5
Yury Petukhou <petukhov.yury@gmail.com>
2019-08-13 23:30:53 +0300

    Added dir [routes, controlles]

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const{port, baseUri} = require('./dbconfig');
 const mongoose = require("mongoose");
-
-
 const allUsers = require('./routes/allUsers');
-
 const app = express();
+const userRout = require('./routes/userRouter');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
 app.use('/api/allUsers', allUsers);
+app.use('/api/users', userRout);
 
 mongoose.connect(baseUri, {useNewUrlParser: true}, (err) => {
     if(err) return console.error(err);
