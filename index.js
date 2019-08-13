fc6cd17d25850f93488ca7dd12c99acb932e45c7
Yury Petukhou <Yury.Petukhou@itechart-group.com>
2019-08-13 11:14:18 +0300

    Change uri path

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const{port, baseUri} = require('./dbconfig');
 const mongoose = require("mongoose");
+const Admin = require('./models/mainAdmin');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
-
 mongoose.connect(baseUri, {useNewUrlParser: true}, (err) => {
     if(err) return console.error(err);
 
     app.listen(port, (err) => {
         if(err) {
-            console.error(`Server drop `)
+            console.error(`Server drop`)
         }
         console.log(`Server up on ${port}`)
     })
     
-} )
+});
