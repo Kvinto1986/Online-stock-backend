64ba98c05e5c6465dfd5f4ac1c5aa27277749cdc
Yan.Palyn <Kvinto1986@gmail.com>
2019-08-14 14:59:42 +0300

    add login search

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const{port, baseUri} = require('./dbconfig');
 const mongoose = require("mongoose");
 
 
-const allUsers = require('./routes/allUsers');
+const allUsers = require('./routes/allUsersRoute');
+const users = require('./routes/userRoute');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
 app.use('/api/allUsers', allUsers);
+app.use('/api/users', users);
 
 mongoose.connect(baseUri, {useNewUrlParser: true}, (err) => {
     if(err) return console.error(err);
