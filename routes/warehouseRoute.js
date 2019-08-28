b7378e7af2bcda20a8e3d9b9f89acb2185cb4164
Yan.Palyn <Kvinto1986@gmail.com>
2019-08-28 12:36:51 +0300

    Merge branch 'development' of D:\Online-stock-backend with conflicts.

---
+++ routes/warehouseRoute.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const WarehousesRoute = express.Router();
+const {addWarehouse} = require('../controlles/warehouseControllers');
+
+WarehousesRoute.post('/registration', addWarehouse);
+
+module.exports = WarehousesRoute;
