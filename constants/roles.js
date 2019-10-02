1e92391ace2ab8f9cf00872ee9ea84df61236637
Yan.Palyn <Kvinto1986@gmail.com>
2019-10-02 10:16:08 +0300

    fix add roles

--- constants/roles.js
+++ constants/roles.js
@@ -1,4 +1,6 @@
 module.exports.COMPANY_ADMIN='companyAdmin';
 module.exports.EMPLOYEE='employee';
 module.exports.MAIN_ADMIN='mainAdmin';
-
+module.exports.EMPLOYEE_OPERATOR = 'operator';
+module.exports.EMPLOYEE_CONTROLLER = 'controller';
+module.exports.EMPLOYEE_MANAGER = 'manager';
--- routes/warehouseRoute.js
+++ routes/warehouseRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middlewares/authorization/passport.middlware');
-const {COMPANY_ADMIN, EMPLOYEE} = require('../constants/roles');
+const {COMPANY_ADMIN, EMPLOYEE, EMPLOYEE_MANAGER} = require('../constants/roles');
 const {createWarehouse, editWarehouse, getWarehouses, getWarehouse, deleteWarehouse} = require('../controllers/warehouseControllers');
 
 router.post('', auth.AUTH([COMPANY_ADMIN]), createWarehouse);
-router.post('/:id', auth.AUTH([COMPANY_ADMIN, EMPLOYEE]), editWarehouse);
-router.get('', auth.AUTH([COMPANY_ADMIN,EMPLOYEE]), getWarehouses);
-router.get('/:id', auth.AUTH([COMPANY_ADMIN, EMPLOYEE]), getWarehouse);
+router.post('/:id', auth.AUTH([COMPANY_ADMIN, EMPLOYEE], EMPLOYEE_MANAGER), editWarehouse);
+router.get('', auth.AUTH([COMPANY_ADMIN, EMPLOYEE], EMPLOYEE_MANAGER), getWarehouses);
+router.get('/:id', auth.AUTH([COMPANY_ADMIN, EMPLOYEE], EMPLOYEE_MANAGER), getWarehouse);
 router.delete('/:id', auth.AUTH([COMPANY_ADMIN]), deleteWarehouse);
 
 module.exports = router;
