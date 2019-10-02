f33def64434d74f639f620ffbee6b76e29a042b9
Yan.Palyn <Kvinto1986@gmail.com>
2019-10-02 10:00:28 +0300

    fix password

--- middlewares/authorization/passport.middlware.js
+++ middlewares/authorization/passport.middlware.js
@@ -1,11 +1,22 @@
 const passport = require('../../passport');
+const {EMPLOYEE} = require('../../constants/roles');
 
-module.exports.AUTH = (roles) => (req, res, next) => {
-    passport.authenticate('jwt', { session: false }, function(err, user) {
-        if (err) { return next(err) }
-        if (!user) { return res.send("Custom Unauthorised").end() }
+module.exports.AUTH = (availableRoles, employeePosition) => (req, res, next) => {
+    passport.authenticate('jwt', {session: false}, function (err, user) {
+        if (err) {
+            return next(err)
+        }
+        if (!user) {
+            return res.send("Custom Unauthorised").end()
+        }
 
-        if (!roles.includes(user.role)) {
+        if (!availableRoles.includes(user.role)) {
+            return res.status(403).json({
+                user: 'This request is not available to you'
+            })
+        }
+
+        if (availableRoles.includes(EMPLOYEE) && !user.position.includes(employeePosition)) {
             return res.status(403).json({
                 user: 'This request is not available to you'
             })
